06d0f7ecf303c9e56e789755d246c4177955540a
HChong <hchong7557@gmail.com>
2016-04-21 21:34:06 +0800

    test
    test

---
+++ CustomForm/CustomFormController.h
@@ -0,0 +1,13 @@
+//
+//  CustomFormController.h
+//  CustomForm
+//
+//  Created by HChong on 16/4/21.
+//  Copyright © 2016年 HChong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CustomFormController : CustomBaseCell
+
+@end
---
+++ CustomForm/CustomFormController.m
@@ -0,0 +1,13 @@
+//
+//  CustomFormController.m
+//  CustomForm
+//
+//  Created by HChong on 16/4/21.
+//  Copyright © 2016年 HChong. All rights reserved.
+//
+
+#import "CustomFormController.h"
+
+@implementation CustomFormController
+
+@end
