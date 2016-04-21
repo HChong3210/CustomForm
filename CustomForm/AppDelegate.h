6af2680c258c336d4c3bf30674a4bf202837ce70
HChong <hchong7557@gmail.com>
2016-04-21 21:31:39 +0800

    Initial Commit

---
+++ CustomForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomForm
+//
+//  Created by HChong on 16/4/21.
+//  Copyright © 2016年 HChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
